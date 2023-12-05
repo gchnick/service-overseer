10af1c23cff99279dd7c36280b2b208a1775ce92
Nick Galán <gnick.alan@gmail.com>
2023-12-05 14:17:08 -0400

    working in show territories

--- src/app/modules/territory/pages/territory-card/territory-card.component.ts
+++ src/app/modules/territory/pages/territory-card/territory-card.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import {
+  ChangeDetectionStrategy,
   Component,
   EventEmitter,
   HostBinding,
@@ -8,20 +9,24 @@ import {
   Output,
   booleanAttribute,
 } from '@angular/core';
-import { UiCardComponent } from '@nikosoftware/core-ui';
+import {
+  UiAvatarHeaderComponent,
+  UiCardComponent,
+} from '@nikosoftware/core-ui';
 
 @Component({
   selector: 'app-territory-card',
   standalone: true,
-  imports: [CommonModule, UiCardComponent],
+  imports: [DatePipe, UiCardComponent, UiAvatarHeaderComponent],
   templateUrl: './territory-card.component.html',
   styleUrls: ['./territory-card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TerritoryCardComponent {
   @Output() cardClick = new EventEmitter<number>();
-  @Input({ required: true }) id!: string;
   @Input({ required: true }) number!: number;
-  @Input({ required: true }) label!: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) lastDateCompleted!: Date;
   @Input({ transform: booleanAttribute }) selected!: boolean;
 
   @HostBinding('class') get clazz() {
